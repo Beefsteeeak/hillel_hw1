2a52b7db0f921970a15bbd4dd6ec60de6ad3e11a
Stas <stnslvrzhk@gmail.com>
2021-05-22 20:11:04 +0300

    number 1

--- app.py
+++ app.py
@@ -8,5 +8,12 @@ def hello_world():
     return 'Hello World!'
 
 
+@app.route('/requirements/')
+def show_requirements():
+    with open("requirements.txt", "r") as r:
+        g = r.read()
+        g = g.replace('\n', '<br>')
+    return g
+
 if __name__ == '__main__':
     app.run()
