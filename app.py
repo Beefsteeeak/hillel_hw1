07b19acfc4b3f1101c79bd6df5dc8f4c22bcfd2c
Stas <stnslvrzhk@gmail.com>
2021-05-23 13:59:14 +0300

    number 2 and updated requirements.txt

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask
+from flask import request
+from faker import Faker
+
+fake = Faker()
 
 app = Flask(__name__)
 
@@ -8,12 +12,31 @@ def hello_world():
     return 'Hello World!'
 
 
+# task1
 @app.route('/requirements/')
 def show_requirements():
+    superline = ""
     with open("requirements.txt", "r") as r:
-        g = r.read()
-        g = g.replace('\n', '<br>')
-    return g
+        for line in r:
+            superline += line
+    superline = superline.replace('\n', '<br>')
+    return superline
+# another option
+#        g = r.read()
+#        g = g.replace('\n', '<br>')
+#    return g
+
+
+# task2
+@app.route('/generate-users/')
+def generate_users():
+    n = min(int((request.args.get("count", 100))), 100)
+    superline = ""
+    for _ in range(n):
+        superline += fake.name() + ' ' + fake.name().lower().replace(' ', '') + '@mail.com' + '\n'
+    superline = superline.replace('\n', '<br>')
+    return superline
+
 
 if __name__ == '__main__':
     app.run()
